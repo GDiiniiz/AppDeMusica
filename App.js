4d33d91afd5e8727b2809cff2ab754a683344074
Guilherme Diniz <guii@192.168.1.4>
2023-10-02 15:11:37 -0300

    atualizado

--- App.js
+++ App.js
@@ -1,20 +1,190 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ImagePropTypes, LogBox, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Audio} from 'expo-av';
+import {AntDesign} from '@expo/vector-icons';
+import Player from './Player.js';
 
 export default function App() {
+
+  LogBox.ignoreAllLogs(true);
+
+  const [audioIndex,setarAudioIndex] = useState(0);
+
+  const [playing,setPlaying] = useState(false);
+  
+  const [audio,setarAudio] = useState(null);
+
+  const [musicas,setarMusicas] = useState([
+
+    {
+      nome: "Stay",
+      artista: "The Kid LAROI & Justin Bieber",
+      playing: false,
+      file: {uri:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'}
+    },
+     
+    {
+      nome: "Levitating",
+      artista: "Dua Lipa",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Montero (Call Me By Your Name)",
+      artista: "Lil Nas X",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Good 4 U",
+      artista: "Olivia Rodrigo",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Kiss Me More",
+      artista: "Doja Cat ft. SZA",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Butter",
+      artista: " BTS",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Deja Vu",
+      artista: " Olivia Rodrigo",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Save Your Tears",
+      artista: "The Weeknd & Ariana Grande (Remix)",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Blinding Lights",
+      artista: "The Weeknd",
+      playing: false,
+      file: ''
+    },
+    {
+      nome: "Peaches",
+      artista: "Justin Bieber ft. Daniel Caesar & Giveon",
+      playing: false,
+      file: ''
+    }
+  ]);
+
+  const changeMusic = async (id) =>{
+      let curFile = null;
+      let newMusics = musicas.filter((val,k)=>{
+            if(id == k){
+                musicas[k].playing = true;
+               
+                curFile = musicas[k].file;
+                setPlaying(true);
+                setarAudioIndex(id);
+            }
+            else{
+                musicas[k].playing = false;
+            }
+
+            return musicas[k];
+      })
+
+      if(audio != null){
+          audio.unloadAsync();
+      }
+
+      let curAudio = new Audio.Sound();
+
+      try{
+          await curAudio.loadAsync(curFile);
+          await curAudio.playAsync();
+      }catch(error){}
+
+      setarAudio(curAudio);
+      setarMusicas(newMusics);
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <View style={{flex:1}}>
+      <ScrollView style={styles.container}>
+          <StatusBar hidden />
+          <View style={styles.header}>
+            <Text style={{textAlign:'center',color:'white',fontSize:25, marginTop: 21}}>App Música</Text>
+          </View>
+
+          <View style={styles.table}>
+              <Text style={{width:'50%',color:'rgb(200,200,200)'}}>Música</Text>
+              <Text style={{width:'50%',color:'rgb(200,200,200)'}}>Artista</Text>
+          </View>
+
+
+          {
+            musicas.map((val,k)=>{
+                
+                if(val.playing){
+                    //Renderiza algo aqui.
+                    return(
+                    <View style={styles.table}>
+                        <TouchableOpacity onPress={()=>changeMusic(k)}  style={{width:'100%',flexDirection:'row'}}>
+                            <Text style={styles.tableTextSelected}><AntDesign name="play" size={15} 
+                            color="#1DB954" /> {val.nome}</Text>
+                            <Text style={styles.tableTextSelected}>{val.artista}</Text>
+                        </TouchableOpacity>
+                    </View>
+                    );
+                }else{
+                  //Renderiza outra coisa aqui.
+                  return(
+                    <View style={styles.table}>
+                        <TouchableOpacity onPress={()=>changeMusic(k)} style={{width:'100%',flexDirection:'row'}}>
+                            <Text style={styles.tableText}><AntDesign name="play" size={15} 
+                            color="white" /> {val.nome}</Text>
+                            <Text style={styles.tableText}>{val.artista}</Text>
+                        </TouchableOpacity>
+                    </View>
+                    );
+                }
+
+            })
+          }
+
+          
+        <View style={{paddingBottom:200}}></View>
+        
+      </ScrollView>
+      <Player playing={playing}  setPlaying={setPlaying} setarAudioIndex={setarAudioIndex} audioIndex={audioIndex} musicas={musicas}
+        setarMusicas={setarMusicas} audio={audio} setarAudio={setarAudio}
+      ></Player>
+      </View>
+      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#222'
   },
+  header:{
+    backgroundColor:'#1DB954',
+    width:'100%',
+    padding:20
+  },
+  table:{
+    flexDirection:'row',
+    padding:20,
+    borderBottomColor:'white',
+    borderBottomWidth:1
+  },
+  tableTextSelected:{width:'50%',color:'#1DB954'},
+  tableText:{width:'50%',color:'white'}
 });
